8502834347cccde94a015217e320f952da9e1157
Pablo de Jesús <pag753@gmail.com>
2019-02-08 11:53:17 -0600

    Cambios en API IP.

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -22,20 +22,14 @@ class Welcome extends CI_Controller {
   private function location()
   {
     $ip = '';
-    if (getenv('HTTP_CLIENT_IP'))
-    $ip = getenv('HTTP_CLIENT_IP');
-    else if(getenv('HTTP_X_FORWARDED_FOR'))
-    $ip = getenv('HTTP_X_FORWARDED_FOR');
-    else if(getenv('HTTP_X_FORWARDED'))
-    $ip = getenv('HTTP_X_FORWARDED');
-    else if(getenv('HTTP_FORWARDED_FOR'))
-    $ip = getenv('HTTP_FORWARDED_FOR');
-    else if(getenv('HTTP_FORWARDED'))
-    $ip = getenv('HTTP_FORWARDED');
-    else if(getenv('REMOTE_ADDR'))
-    $ip = getenv('REMOTE_ADDR');
+    if (getenv('HTTP_CLIENT_IP')) $ip = getenv('HTTP_CLIENT_IP');
+    else if(getenv('HTTP_X_FORWARDED_FOR')) $ip = getenv('HTTP_X_FORWARDED_FOR');
+    else if(getenv('HTTP_X_FORWARDED')) $ip = getenv('HTTP_X_FORWARDED');
+    else if(getenv('HTTP_FORWARDED_FOR')) $ip = getenv('HTTP_FORWARDED_FOR');
+    else if(getenv('HTTP_FORWARDED')) $ip = getenv('HTTP_FORWARDED');
+    else if(getenv('REMOTE_ADDR')) $ip = getenv('REMOTE_ADDR');
     else $ip = '127.0.0.1';
-    return file_get_contents("https://ip-api.com/json/$ip");
+    return file_get_contents("http://ip-api.com/json/$ip");
     //return file_get_contents("https://ipapi.co/$ip/json");
   }
   public function index()
